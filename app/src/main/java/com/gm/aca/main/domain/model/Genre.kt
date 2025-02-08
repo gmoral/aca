b0b9287e2e3339cc151b50f0f074f89d2378f082
Guillermo Moral <willy.moral@gmail.com>
2025-02-08 20:51:39 -0300

    Add structure

---
+++ app/src/main/java/com/gm/aca/main/domain/model/Genre.kt
@@ -0,0 +1,9 @@
+package com.gm.aca.main.domain.model
+
+/**
+ * @author Ahmed Guedmioui
+ */
+data class Genre(
+    val genreId: Int,
+    val genreName: String
+)
---
+++ app/src/main/java/com/gm/aca/main/domain/model/Media.kt
@@ -0,0 +1,24 @@
+package com.gm.aca.main.domain.model
+
+/**
+ * @author Ahmed Guedmioui
+ */
+data class Media(
+    val mediaId: Int,
+
+    val adult: Boolean,
+    val backdropPath: String,
+    val genreIds: List<String>,
+    val mediaType: String,
+    val originCountry: List<String>,
+    val originalLanguage: String,
+    val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    val posterPath: String,
+    val releaseDate: String,
+    val title: String,
+    val voteAverage: Double,
+    val voteCount: Int,
+    var category: String
+)
